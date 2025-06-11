3efd5c64cf6599f4a47d097fa9a35bf51db48bf8
Brima225 <bkargbo065@central.edu.sl>
2025-06-11 23:23:00 +0000

    Add .next/static/chunks/src_app_admin_dashboard_page_tsx_af5cd94a._.js 1749684178761

---
+++ .next/static/chunks/src_app_admin_dashboard_page_tsx_af5cd94a._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d19e6e9a._.js",
+  "static/chunks/src_3a19765b._.js"
+],
+    source: "dynamic"
+});
